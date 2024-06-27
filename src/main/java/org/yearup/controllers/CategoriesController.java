8eccef2e92f00d3da2065542775938d3b9d6e9d1
DemondJr <djacksonjr@pgh.yearup.org>
2024-06-27 15:43:02 -0400

    controller updated

--- src/main/java/org/yearup/controllers/CategoriesController.java
+++ src/main/java/org/yearup/controllers/CategoriesController.java
@@ -1,5 +1,6 @@
 package org.yearup.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.yearup.data.CategoryDao;
 import org.yearup.data.ProductDao;
@@ -9,11 +10,13 @@ import org.yearup.models.Product;
 import java.util.List;
 
 // add the annotations to make this a REST controller
-// add the annotation to make this controller the endpoint for the following url
-    // http://localhost:8080/categories
-// add annotation to allow cross site origin requests
 @RestController
-@RequestMapping ("categories")
+// add the annotation to make this controller the endpoint for the following url
+@RequestMapping ("/categories")
+
+// http://localhost:8080/categories
+// add annotation to allow cross site origin requests
+@CrossOrigin
 public class CategoriesController
 {
     private CategoryDao categoryDao;
@@ -21,17 +24,23 @@ public class CategoriesController
 
 
     // create an Autowired controller to inject the categoryDao and ProductDao
+@Autowired CategoriesController(CategoryDao categoryDao, ProductDao productDao)
+{   this.categoryDao = categoryDao;
+    this.productDao = productDao;
 
+}
     // add the appropriate annotation for a get action
+    @RequestMapping(method = RequestMethod.GET)
     public List<Category> getAll()
     {
         // find and return all categories
-        return null;
+        return categoryDao.getAllCategories();
     }
 
     // add the appropriate annotation for a get action
+    @GetMapping(path = "/{id}")
     public Category getById(@PathVariable int id)
-    {
+    {   Category category = categoryDao.getById(id);
         // get the category by id
         return null;
     }
